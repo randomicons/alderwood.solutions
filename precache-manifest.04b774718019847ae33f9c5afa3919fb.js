c36693bfaaa209ef487d928454e8fb0190e729ed
Sriram Bhat <srirambhat7@gmail.com>
2020-08-10 18:36:05 -0500

    Updates

---
+++ precache-manifest.04b774718019847ae33f9c5afa3919fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d5accec5b9486620144454c9d1df31",
+    "url": "/alderwood.solutions/index.html"
+  },
+  {
+    "revision": "d0e40f7a16a7cd068d13",
+    "url": "/alderwood.solutions/static/css/main.f409c74d.chunk.css"
+  },
+  {
+    "revision": "810e691d6505bdb561e3",
+    "url": "/alderwood.solutions/static/js/2.a2ebcadc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alderwood.solutions/static/js/2.a2ebcadc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e40f7a16a7cd068d13",
+    "url": "/alderwood.solutions/static/js/main.75c1b1c3.chunk.js"
+  },
+  {
+    "revision": "71749ec0493f0066423b",
+    "url": "/alderwood.solutions/static/js/runtime-main.789dec0f.js"
+  },
+  {
+    "revision": "7d6a3cd309d08c8676caea3a5308c330",
+    "url": "/alderwood.solutions/static/media/home.7d6a3cd3.svg"
+  },
+  {
+    "revision": "cbd2767d35417e7afabe400f99f8ee32",
+    "url": "/alderwood.solutions/static/media/joustr.cbd2767d.png"
+  },
+  {
+    "revision": "5aa85cd5b3a2794a16c3707025067e96",
+    "url": "/alderwood.solutions/static/media/quotr.5aa85cd5.png"
+  }
+]);
